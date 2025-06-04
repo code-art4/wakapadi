987dade40b5b1a780d48db357d8401515aac3253
Samuel Egbajie (MAJ0R) <49847624+CodePapi@users.noreply.github.com>
2025-06-04 14:47:23 +0200

    fix: FE deployment fixes

--- frontend/lib/api/index.ts
+++ frontend/lib/api/index.ts
@@ -18,31 +18,4 @@ api.interceptors.request.use((config) => {
   }
   return config;
 });
-
-export default async function cityHandler(
-  req: { query: { lat: any; lon: any } },
-  res: {
-    status: (arg0: number) => {
-      (): any;
-      new (): any;
-      json: { (arg0: { error?: string; city?: any }): any; new (): any };
-    };
-  }
-) {
-  const { lat, lon } = req.query;
-
-  try {
-    const res = await api.get(`/geolocation/reverse?lat=${lat}&lon=${lon}`);
-
-    const geocode = res.data;
-    const city = (geocode.address.city || geocode.address.town || '')
-      .trim()
-      .toLowerCase();
-
-    return res.status(200).json({ city });
-  } catch (error) {
-    return res.status(500).json({ error: 'Server error' });
-  }
-}
-
-export { api, cityHandler };
+export { api };
