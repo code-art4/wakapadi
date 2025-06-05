8ebf2f06b5fca6240bd3dbeb053e31076510e77b
Samuel Egbajie (MAJ0R) <49847624+CodePapi@users.noreply.github.com>
2025-06-05 09:39:35 +0200

    updated to allow CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,3 @@
-
 import { ReadableStream } from 'web-streams-polyfill';
 import { webcrypto } from 'crypto';
 
@@ -10,15 +9,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-
   if (!globalThis.crypto) {
     globalThis.crypto = webcrypto as unknown as Crypto;
   }
-  
 
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: [process.env.FRONTEND_URL!],
+    origin: [
+      process.env.FRONTEND_URL!,
+      'https://wakapadi.vercel.app',
+      'http://localhost:3001',
+      'http://localhost:3000',
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 3000);
