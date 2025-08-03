e64beb562644cf014289a51d055003a48bb1b86b
Triumph Ojo <ojotriumph4@gmail.com>
2025-08-03 10:35:46 +0100

    update:: fixed the navigation in the overlay nav

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -41,7 +41,7 @@ export default function HomePage() {
   useEffect(() => {
     const mediaQuery = window.matchMedia("(max-width: 599px)");
 
-    const handleChange = (e: any) => setIsMobile(e.matches);
+    const handleChange = (e: MediaQueryListEvent) => setIsMobile(e.matches);
     mediaQuery.addEventListener("change", handleChange);
 
     // Initial check
